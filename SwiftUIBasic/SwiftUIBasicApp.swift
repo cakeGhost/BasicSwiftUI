e2e3c0fa7320f062b608adce571de7d19f9e2a55
suding <betty.sujin.lee@gmail.com>
2021-12-15 14:48:04 +0900

    Initial Commit

---
+++ SwiftUIBasic/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  SwiftUIBasic
+//
+//  Created by suding on 2021/12/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
